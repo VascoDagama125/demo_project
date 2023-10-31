8aa0d17a94f8aa79803a4f0b0a1b5fd820c8dfc9
Paul <paul.dyngosz@gmail.com>
2023-10-31 22:48:48 +0100

    figure calculator first record

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 figure = input("What figure do you want to calculate? :")
 figures = ["square", "triangle", "circle", "rectangle", "elipse", "trapezoid"]
 
@@ -12,6 +13,10 @@ if figure in figures:
         h = int(input("type in measurement of a height:"))
         triangle_area = (a_triangle * h) / 2
         print(f"The area of the square is: {triangle_area}")
+    elif figure in figures and figure == figures[2]:
+        r = int(input("type in measurement of a radius: "))
+        circle_area = math.pi * (r * r)
+        print(f"The area of the square is: {circle_area}")
     else:
         print("Sorry! There is no such option. ")
 else:
